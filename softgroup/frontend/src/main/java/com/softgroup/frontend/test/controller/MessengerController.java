ee1216e1c076714c4057d2601e283081a51c8436
Javier <armenpollo@hotmail.com>
2017-04-06 13:17:20 +0300

    refactor dao after saturday lesson

--- softgroup/frontend/src/main/java/com/softgroup/frontend/test/controller/MessengerController.java
+++ softgroup/frontend/src/main/java/com/softgroup/frontend/test/controller/MessengerController.java
@@ -1,7 +1,5 @@
 package com.softgroup.frontend.test.controller;
-import com.softgroup.common.datamapper.DataMapper;
-import com.softgroup.common.protocol.Response;
-import com.softgroup.common.router.api.Handler;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.charset.StandardCharsets;
 
 
 
@@ -19,26 +16,9 @@ import java.nio.charset.StandardCharsets;
 
 
 @RestController
-/*@RequestMapping(path = "/messenger")*/
-@RequestMapping(path = "/test"/*,
-        method = RequestMethod.POST,
-        consumes = MediaType.TEXT_PLAIN_VALUE,
-        produces = MediaType.TEXT_PLAIN_VALUE*/)
+@RequestMapping(path = "/test")
 public class MessengerController {
 
-/*
-    @Autowired
-    Handler firstRouterHandler;
-
-    @Autowired
-    DataMapper jacksonDataMapper;*/
-
-   /* @RequestMapping(path = "/registration",
-            method = RequestMethod.POST)
-    public Response publicMessage(@RequestBody final String requestStr) {
-        CommonRequest request = jacksonDataMapper.mapData(requestStr.getBytes(StandardCharsets.UTF_8),CommonRequest.class);
-        return firstRouterHandler.handle(request);
-    }*/
 
     @RequestMapping(path = "/message",
             method = RequestMethod.GET)
