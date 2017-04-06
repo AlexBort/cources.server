a2efabeb501f29600ab7cf13dc40c928e79f5a8c
Javier <armenpollo@hotmail.com>
2017-04-06 13:59:21 +0300

    fixing after comments to this branch

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/FRouterHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/FRouterHandler.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FRouterHandler implements Handler {
+public abstract class FRouterHandler implements Handler {
 
 
     @Override
