b432e8b130d024a82081b940cd883f7bb29f0890
Javier <armenpollo@hotmail.com>
2017-04-12 13:18:17 +0300

    factory

--- softgroup/services/authorization/authorization-imp/src/main/java/com/softgroup/authorithation/impl/router/AuthorizationRouter.java
+++ softgroup/services/authorization/authorization-imp/src/main/java/com/softgroup/authorithation/impl/router/AuthorizationRouter.java
@@ -4,9 +4,8 @@ import com.softgroup.authorization.api.router.AuthorizationRequestHandler;
 import com.softgroup.common.router.api.AbstractCommandRouterHandler;
 import com.softgroup.common.router.api.CommandTypeRouter;
 
-/**
- * Created by User on 21.03.2017.
- */
+
+
 public class AuthorizationRouter extends AbstractCommandRouterHandler<AuthorizationRequestHandler> implements CommandTypeRouter {
 
     @Override
