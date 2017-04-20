fef06750f3d687b31cc61e598d06a623c337c85e
Javier <armenpollo@hotmail.com>
2017-04-20 17:04:27 +0300

    working with jwt

---
+++ softgroup/basis/common-jwt/common-jwt-api/src/main/java/com/softgroup/common/jwt/api/service/TokenType.java
@@ -0,0 +1,6 @@
+package com.softgroup.common.jwt.api.service;
+
+
+public enum TokenType {
+    SESSION_TOKEN, DEVICE_TOKEN
+}
---
+++ softgroup/basis/common-jwt/common-jwt-api/src/main/java/com/softgroup/common/jwt/api/service/UniversalTokenService.java
@@ -0,0 +1,9 @@
+package com.softgroup.common.jwt.api.service;
+
+
+public interface UniversalTokenService extends DeviceTokenService, SessionTokenService {
+    Long getTimeOfCreation(String token);
+    TokenType getTokenType(String token);
+    String getDeviceID(String token);
+    String getProfileID(String token);
+}
